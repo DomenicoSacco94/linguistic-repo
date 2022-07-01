f1b96a9e259a9815de345d4df5e7a188bd5f4c1e
DomenicoSacco94 <domenico@element.in>
2022-07-01 13:06:09 +0200

    add text for gui reader

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -6,3 +6,24 @@ describe('Links are rendering', () => {
     cy.contains('Read')
   })
 })
+
+describe('Input is working', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('textarea').type('Hallo Welt. Wieder, hallo Welt')
+    cy.get('.navLink').contains('Read').click()
+    cy.get('div').contains('Hallo Welt')
+    cy.get('div').contains('Wieder, hallo Welt')
+
+    cy.get('div').get('button').contains('Hallo').should('not.be.disabled')
+    cy.get('div').get('button').contains('Hallo').click()
+    cy.get('div').get('button').contains('Hi').should('not.be.disabled')
+    cy.get('div').get('button').contains('Hi').click()
+    cy.get('div').get('button').contains('Hallo').should('not.be.disabled')
+
+    cy.get('div').get('.paragraphButton').eq(0).click()
+    cy.get('div').contains('Hello')
+    cy.get('div').contains('World')
+
+  })
+})
