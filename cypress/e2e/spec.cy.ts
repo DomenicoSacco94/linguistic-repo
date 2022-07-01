81dd93986c5ef4b75c9a78d9e5898fa0bd3d08c2
DomenicoSacco94 <domenico@element.in>
2022-07-01 17:07:11 +0200

    add e2e tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -53,6 +53,57 @@ describe('File upload is working', () => {
     })
 })
 
+describe('Uploading a file after the input works correctly', () => {
+    it('passes', () => {
+        interceptTranslationRequest('Die Deutsche Demokratische Republik', 'Translation of the sentence','sentenceTranslation')
+        cy.visit('http://localhost:3000/')
+        cy.get('textarea').type('German sentence 1. German sentence 2')
+        cy.get('.navLink').contains('Read').click()
+        cy.get('div').contains('German sentence 1')
+        cy.get('div').contains('German sentence 2')
+
+        cy.get('.navLink').contains('Import TXT').click()
+        cy.fixture('test.txt').then(fileContent => {
+            cy.get('input[type="file"]').attachFile({
+                fileContent: fileContent.toString(),
+                fileName: 'testPicture.png',
+                mimeType: 'image/png'
+            });
+        });
+
+
+        cy.get('.navLink').contains('Input').click()
+        cy.get('div').contains('German sentence 1').should('not.exist');
+
+        cy.get('.navLink').contains('Read').click()
+        cy.get('div').contains('Die Deutsche Demokratische Republik')
+    })
+})
+
+describe('Input text after uploading a file works correctly', () => {
+    it('passes', () => {
+        interceptTranslationRequest('Die Deutsche Demokratische Republik', 'Translation of the sentence','sentenceTranslation')
+        cy.visit('http://localhost:3000/')
+
+        cy.get('.navLink').contains('Import TXT').click()
+        cy.fixture('test.txt').then(fileContent => {
+            cy.get('input[type="file"]').attachFile({
+                fileContent: fileContent.toString(),
+                fileName: 'testPicture.png',
+                mimeType: 'image/png'
+            });
+        });
+        cy.get('.navLink').contains('Read').click()
+        cy.get('div').contains('Die Deutsche Demokratische Republik')
+
+        cy.get('.navLink').contains('Input').click()
+        cy.get('textarea').type('German sentence 1. German sentence 2')
+        cy.get('.navLink').contains('Read').click()
+        cy.get('div').contains('German sentence 1')
+        cy.get('div').contains('German sentence 2')
+    })
+})
+
 const interceptTranslationRequest = (toTranslate: string, translated: string, description: string) => {
     cy.intercept('https://translate.googleapis.com/**', {
             statusCode: 201,
