8a7054c72854649d905ac7c6fb065ea68c9e3e27
DomenicoSacco94 <domenico@element.in>
2022-07-01 15:48:56 +0200

    add cypress test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,29 +1,49 @@
 describe('Links are rendering', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
-    cy.contains('Import TXT')
-    cy.contains('Input')
-    cy.contains('Read')
-  })
+    it('passes', () => {
+        cy.visit('http://localhost:3000/')
+        cy.contains('Import TXT')
+        cy.contains('Input')
+        cy.contains('Read')
+    })
 })
 
 describe('Input is working', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('textarea').type('Hallo Welt. Wieder, hallo Welt')
-    cy.get('.navLink').contains('Read').click()
-    cy.get('div').contains('Hallo Welt')
-    cy.get('div').contains('Wieder, hallo Welt')
+    it('passes', () => {
+        cy.visit('http://localhost:3000/')
+        cy.get('textarea').type('Hallo Welt. Wieder, hallo Welt')
+        cy.get('.navLink').contains('Read').click()
+        cy.get('div').contains('Hallo Welt')
+        cy.get('div').contains('Wieder, hallo Welt')
 
-    cy.get('div').get('button').contains('Hallo').should('not.be.disabled')
-    cy.get('div').get('button').contains('Hallo').click()
-    cy.get('div').get('button').contains('Hi').should('not.be.disabled')
-    cy.get('div').get('button').contains('Hi').click()
-    cy.get('div').get('button').contains('Hallo').should('not.be.disabled')
+        cy.get('div').get('button').contains('Hallo').should('not.be.disabled')
+        cy.get('div').get('button').contains('Hallo').click()
+        interceptTranslationRequest('Hallo', 'Hi')
+        cy.get('div').get('button').contains('Hi').should('not.be.disabled')
+        cy.get('div').get('button').contains('Hi').click()
+        cy.get('div').get('button').contains('Hallo').should('not.be.disabled')
 
-    cy.get('div').get('.paragraphButton').eq(0).click()
-    cy.get('div').contains('Hello')
-    cy.get('div').contains('World')
+        cy.get('div').get('.paragraphButton').eq(0).click()
+        interceptTranslationRequest('Hallo Welt', 'Hello World')
+        cy.get('div').contains('Hello')
+        cy.get('div').contains('World')
 
-  })
+    })
 })
+
+const interceptTranslationRequest = (toTranslate: string, translated: string) => {
+    cy.intercept('https://translate.googleapis.com/translate_a/!*', {
+            statusCode: 201,
+            body: {
+                data: [
+                    [
+                        toTranslate,
+                        translated,
+                        null,
+                        null,
+                        1
+                    ]
+                ]
+            }
+        }
+    )
+}
