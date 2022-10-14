160827d4b0023df067fbe311f55577cd645e2242
DomenicoSacco94 <domenico@element.in>
2022-10-14 15:47:07 +0200

    add provisional clear cache button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { SiteRoutes } from './Index';
 import { IndexItem } from '../types/IndexItem';
-import { ROOT_PATH } from '../utils/constants';
+import {
+  ROOT_PATH,
+  SAVED_TEXT_KEY,
+  SAVED_TRANSLATIONS_KEY,
+} from '../utils/constants';
+import { Button } from 'antd';
 
 export const Navigation = () => {
   return (
     <nav className="navBar">
+      <Button
+        onClick={() => {
+          localStorage.removeItem(SAVED_TRANSLATIONS_KEY);
+          localStorage.removeItem(SAVED_TEXT_KEY);
+        }}
+      >
+        Clear Cache
+      </Button>
       {SiteRoutes.map(
         (route: IndexItem) =>
           !route.hide && (
